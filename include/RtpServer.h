ce52bde2011d0cab19e0af51db716da426f85f02
ori cohen <oric498@gmail.com>
2024-10-20 12:20:49 +0300

    BYTE_DEPTH -> BIT_DEPTH / SIZEOF(FLOAT)

--- include/RtpServer.h
+++ include/RtpServer.h
@@ -9,7 +9,7 @@ class RtpServer
 public:
     static const int RTP_HEADER_SIZE = 12;
     static const int MAX_BUFFER_LENGTH = 256;
-    static const int BYTE_DEPTH = 4;
+    static const int BIT_DEPTH = 16;
 
 public:
     RtpServer(boost::asio::io_context &io_ctx, unsigned short port);
@@ -21,7 +21,7 @@ private:
     boost::asio::ip::udp::socket m_socket;
     enum
     {
-        MAX_LENGTH = RTP_HEADER_SIZE + MAX_BUFFER_LENGTH * BYTE_DEPTH
+        MAX_LENGTH = RTP_HEADER_SIZE + MAX_BUFFER_LENGTH * BIT_DEPTH / sizeof(float)
     };
     uint8_t m_data[MAX_LENGTH];
 };
